6dee80b4f8d711bcec1de7ead83e1b45f86ff0c6
Callum Davidson <callum.davidson53@gmail.com>
2022-11-26 15:38:22 +0000

    Notes added to index

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,24 @@ const [spots, reserve, occupy, occupyingCar, owns] = CreateArrays(); //Call Crea
 
 const specSpot = 'A9'; //Spot hard-coded as A9 - gets fed into findSpotInfo below
 
+// Someone says I want A10
+// Class that handles reservation requests takes that
+//    - takes spot number(if applicable) / start date-time / end date-time / type of spot requested (if applicable) / car info (if applicable) / name of requester
+// Hit db for info on A10 (create db access class so there's one point of entry for db)
+// db access class grabs data and creates parking spot object (new Parking Spot)
+// ParkingSpot class could then be returned back
+
+// db access class
+//    - only job is to hit the db and return info
+//    - can return an array of created class objects
+//    - return it as a vehicle interface
+//    - pass it to a create a vehicle type identifier class which casts to the correct type of vehicle class
+//          - look up creating a builder class which would create the car/lorry etc. object for me and check that everything is how it should be
+
+// use a for each loop to go through an array of created spot objects
+
 const [resStatus, occStatus, occCar, owner] = findSpotInfo( //call findSpotInfo to get the reservation status, occupied status, occupying car, and car owner
-  specSpot,
+  specSpot, // needs to pull from class not from database
   spots,
   reserve,
   occupy,
@@ -31,6 +47,8 @@ export const clientVeh = new Car( //initialises the Car - pulls in the occupying
   2000,
   2000
 ); //type,make,model,reg,owner,electric
+// create car then write it to database so it isn't overwritten
+// to pull back from the database, put it back into a new Car const
 
 const pSpot = new ParkingSpot(specSpot, resStatus, occStatus, clientVeh, true); //initialise a new parking spot, pull in the specific spot, reservation status, occupied status, and the vehicle
 //specSpot is hard coded line 11, reservation status is returned from findSpotInfo with occupied status, pulls in all details of the vehicle from clientVeh
@@ -51,7 +69,7 @@ if (pSpot.occupiedStatus == true) {
 //  - create a file in a place
 //    - tell program to create me carpark.csv
 //  - write to that file after it's been created
-// Create other vehicle classes
+// Create other vehicle classes - Done
 // Changing parking spot to interface
 // Create other parking spot classes
 // Look up data structures
